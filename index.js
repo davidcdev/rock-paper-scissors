c27a8d0fe951be5a01f3a1f321a8001a0dacbcb0
David Castellanos <128877503+davidcdev@users.noreply.github.com>
2023-09-04 17:14:44 -0500

    Declare functions for pc and player selection

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ let pcWins = 0;
 let playerWins = 0;
 let result = '';
 let finalResult = '';
+
+function getComputersChoice () {
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function playerSelection () {
+  const playerChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
+  return playerChoice;
+}
+
