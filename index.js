70a9774abb10ca9228699141b8bf5ce1a03dd6b6
David Castellanos <128877503+davidcdev@users.noreply.github.com>
2023-09-04 17:26:36 -0500

    Declare a function to play one round

--- index.js
+++ index.js
@@ -15,3 +15,42 @@ function playerSelection () {
   return playerChoice;
 }
 
+function playRound (playerSelection, getComputersChoice) {
+  if (playerSelection === getComputersChoice) result = 'Tie! 👀';
+  
+  if (playerSelection === 'rock') {
+    if (getComputersChoice === 'paper') {
+      pcWins += 1;
+      result = 'You Lose! Paper beats Rock ❌'; 
+    } else if (getComputersChoice === 'scissors') {
+      playerWins += 1;
+      result = 'You Win! Rock beats Scissors 👏'; 
+    }
+  }
+
+  if (playerSelection === 'paper') {
+    if (getComputersChoice === 'rock') {
+      playerWins += 1;
+      result = 'You Win! Paper beats Rock 👏';
+    } else if (getComputersChoice === 'scissors') {
+      pcWins += 1;
+      result = 'You Lose! Scissors beats Paper ❌';
+    }
+  }
+
+  if (playerSelection === 'scissors') {
+    if (getComputersChoice === 'rock') {
+      pcWins += 1;
+      result = 'You Lose! Rock beats Scissors ❌';
+    } else if (getComputersChoice === 'paper') {
+      playerWins += 1;
+      result = 'You Win! Scissors beats Paper 👏';
+    }
+  }
+
+  console.log(`Player selected ${playerSelection}`);
+  console.log(`Computer selected ${getComputersChoice}`);
+  console.log(`Score: Player: ${playerWins} - PC: ${pcWins}`);
+  alert(result);
+  return result;
+}
